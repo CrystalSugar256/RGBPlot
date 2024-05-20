240acfbaa47f567b4cc639477eda860bd87cb659
Sugar <20020612take4@gmail.com>
2024-05-20 16:19:35 +0900

    Xlsx出力対応 使いやすさ向上

--- RGBPlot/Form1.Designer.cs
+++ RGBPlot/Form1.Designer.cs
@@ -23,85 +23,90 @@
         /// コード エディターで変更しないでください。
         /// </summary>
         private void InitializeComponent() {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.ChosePicture = new System.Windows.Forms.Button();
-            this.SetStart = new System.Windows.Forms.Button();
-            this.SetEnd = new System.Windows.Forms.Button();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.ChartBox = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.ChosePictureBtn = new System.Windows.Forms.Button();
+            this.SetStartBtn = new System.Windows.Forms.Button();
+            this.SetEndBtn = new System.Windows.Forms.Button();
             this.filePath = new System.Windows.Forms.Label();
             this.startPos = new System.Windows.Forms.Label();
             this.endPos = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.DrawChartBtn = new System.Windows.Forms.Button();
+            this.PartitionCountBox = new System.Windows.Forms.NumericUpDown();
+            this.Lable_Partition = new System.Windows.Forms.Label();
+            this.SaveExcelFile = new System.Windows.Forms.Button();
+            this.Label_log = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ChartBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PartitionCountBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // chart1
+            // ChartBox
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(524, 12);
-            this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(500, 300);
-            this.chart1.TabIndex = 0;
-            this.chart1.Text = "chart1";
+            chartArea2.Name = "ChartArea1";
+            this.ChartBox.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.ChartBox.Legends.Add(legend2);
+            this.ChartBox.Location = new System.Drawing.Point(524, 12);
+            this.ChartBox.Name = "ChartBox";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.ChartBox.Series.Add(series2);
+            this.ChartBox.Size = new System.Drawing.Size(500, 300);
+            this.ChartBox.TabIndex = 0;
+            this.ChartBox.Text = "chart1";
             // 
-            // pictureBox1
+            // PictureBox
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(471, 300);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.PictureBox.Location = new System.Drawing.Point(12, 12);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(471, 300);
+            this.PictureBox.TabIndex = 1;
+            this.PictureBox.TabStop = false;
+            this.PictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox_MouseClick);
             // 
-            // ChosePicture
+            // ChosePictureBtn
             // 
-            this.ChosePicture.Location = new System.Drawing.Point(12, 341);
-            this.ChosePicture.Name = "ChosePicture";
-            this.ChosePicture.Size = new System.Drawing.Size(133, 48);
-            this.ChosePicture.TabIndex = 2;
-            this.ChosePicture.Text = "ChosePicture";
-            this.ChosePicture.UseVisualStyleBackColor = true;
-            this.ChosePicture.Click += new System.EventHandler(this.button1_Click);
+            this.ChosePictureBtn.Location = new System.Drawing.Point(12, 341);
+            this.ChosePictureBtn.Name = "ChosePictureBtn";
+            this.ChosePictureBtn.Size = new System.Drawing.Size(133, 48);
+            this.ChosePictureBtn.TabIndex = 2;
+            this.ChosePictureBtn.Text = "ChosePicture";
+            this.ChosePictureBtn.UseVisualStyleBackColor = true;
+            this.ChosePictureBtn.Click += new System.EventHandler(this.ChosePictureBtn_Click);
             // 
-            // SetStart
+            // SetStartBtn
             // 
-            this.SetStart.Location = new System.Drawing.Point(247, 341);
-            this.SetStart.Name = "SetStart";
-            this.SetStart.Size = new System.Drawing.Size(81, 48);
-            this.SetStart.TabIndex = 3;
-            this.SetStart.Text = "StartPoint";
-            this.SetStart.UseVisualStyleBackColor = true;
-            this.SetStart.Click += new System.EventHandler(this.button2_Click);
+            this.SetStartBtn.Location = new System.Drawing.Point(247, 341);
+            this.SetStartBtn.Name = "SetStartBtn";
+            this.SetStartBtn.Size = new System.Drawing.Size(81, 48);
+            this.SetStartBtn.TabIndex = 3;
+            this.SetStartBtn.Text = "StartPoint";
+            this.SetStartBtn.UseVisualStyleBackColor = true;
+            this.SetStartBtn.Click += new System.EventHandler(this.SetStartBtn_Click);
             // 
-            // SetEnd
+            // SetEndBtn
             // 
-            this.SetEnd.Location = new System.Drawing.Point(402, 341);
-            this.SetEnd.Name = "SetEnd";
-            this.SetEnd.Size = new System.Drawing.Size(81, 48);
-            this.SetEnd.TabIndex = 4;
-            this.SetEnd.Text = "EndPoint";
-            this.SetEnd.UseVisualStyleBackColor = true;
-            this.SetEnd.Click += new System.EventHandler(this.button3_Click);
+            this.SetEndBtn.Location = new System.Drawing.Point(402, 341);
+            this.SetEndBtn.Name = "SetEndBtn";
+            this.SetEndBtn.Size = new System.Drawing.Size(81, 48);
+            this.SetEndBtn.TabIndex = 4;
+            this.SetEndBtn.Text = "EndPoint";
+            this.SetEndBtn.UseVisualStyleBackColor = true;
+            this.SetEndBtn.Click += new System.EventHandler(this.SetEndBtn_Click);
             // 
             // filePath
             // 
             this.filePath.AutoSize = true;
             this.filePath.Location = new System.Drawing.Point(12, 315);
             this.filePath.Name = "filePath";
-            this.filePath.Size = new System.Drawing.Size(43, 15);
+            this.filePath.Size = new System.Drawing.Size(34, 15);
             this.filePath.TabIndex = 6;
-            this.filePath.Text = "label2";
+            this.filePath.Text = "path";
             // 
             // startPos
             // 
@@ -121,34 +126,80 @@
             this.endPos.TabIndex = 8;
             this.endPos.Text = "label1";
             // 
-            // button1
+            // DrawChartBtn
             // 
-            this.button1.Location = new System.Drawing.Point(524, 341);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(129, 48);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "DrawChart";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.DrawChartBtn.Location = new System.Drawing.Point(524, 341);
+            this.DrawChartBtn.Name = "DrawChartBtn";
+            this.DrawChartBtn.Size = new System.Drawing.Size(129, 48);
+            this.DrawChartBtn.TabIndex = 9;
+            this.DrawChartBtn.Text = "DrawChart";
+            this.DrawChartBtn.UseVisualStyleBackColor = true;
+            this.DrawChartBtn.Click += new System.EventHandler(this.DrawChartBtn_Click);
+            // 
+            // PartitionCountBox
+            // 
+            this.PartitionCountBox.Location = new System.Drawing.Point(682, 366);
+            this.PartitionCountBox.Name = "PartitionCountBox";
+            this.PartitionCountBox.Size = new System.Drawing.Size(120, 22);
+            this.PartitionCountBox.TabIndex = 10;
+            this.PartitionCountBox.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.PartitionCountBox.ValueChanged += new System.EventHandler(this.PartitionCountBox_ValueChanged);
+            // 
+            // Lable_Partition
+            // 
+            this.Lable_Partition.AutoSize = true;
+            this.Lable_Partition.Location = new System.Drawing.Point(679, 348);
+            this.Lable_Partition.Name = "Lable_Partition";
+            this.Lable_Partition.Size = new System.Drawing.Size(52, 15);
+            this.Lable_Partition.TabIndex = 11;
+            this.Lable_Partition.Text = "分割数";
+            // 
+            // SaveExcelFile
+            // 
+            this.SaveExcelFile.Location = new System.Drawing.Point(831, 341);
+            this.SaveExcelFile.Name = "SaveExcelFile";
+            this.SaveExcelFile.Size = new System.Drawing.Size(107, 47);
+            this.SaveExcelFile.TabIndex = 12;
+            this.SaveExcelFile.Text = "SaveAs Xlsx";
+            this.SaveExcelFile.UseVisualStyleBackColor = true;
+            this.SaveExcelFile.Click += new System.EventHandler(this.SaveExcelFile_Click);
+            // 
+            // Label_log
+            // 
+            this.Label_log.AutoSize = true;
+            this.Label_log.Location = new System.Drawing.Point(15, 417);
+            this.Label_log.Name = "Label_log";
+            this.Label_log.Size = new System.Drawing.Size(43, 15);
+            this.Label_log.TabIndex = 13;
+            this.Label_log.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1036, 546);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Label_log);
+            this.Controls.Add(this.SaveExcelFile);
+            this.Controls.Add(this.Lable_Partition);
+            this.Controls.Add(this.PartitionCountBox);
+            this.Controls.Add(this.DrawChartBtn);
             this.Controls.Add(this.endPos);
             this.Controls.Add(this.startPos);
             this.Controls.Add(this.filePath);
-            this.Controls.Add(this.SetEnd);
-            this.Controls.Add(this.SetStart);
-            this.Controls.Add(this.ChosePicture);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.SetEndBtn);
+            this.Controls.Add(this.SetStartBtn);
+            this.Controls.Add(this.ChosePictureBtn);
+            this.Controls.Add(this.PictureBox);
+            this.Controls.Add(this.ChartBox);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ChartBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PartitionCountBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -156,15 +207,19 @@
 
         #endregion
 
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button ChosePicture;
-        private System.Windows.Forms.Button SetStart;
-        private System.Windows.Forms.Button SetEnd;
+        private System.Windows.Forms.DataVisualization.Charting.Chart ChartBox;
+        private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.Button ChosePictureBtn;
+        private System.Windows.Forms.Button SetStartBtn;
+        private System.Windows.Forms.Button SetEndBtn;
         private System.Windows.Forms.Label filePath;
         private System.Windows.Forms.Label startPos;
         private System.Windows.Forms.Label endPos;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button DrawChartBtn;
+        private System.Windows.Forms.NumericUpDown PartitionCountBox;
+        private System.Windows.Forms.Label Lable_Partition;
+        private System.Windows.Forms.Button SaveExcelFile;
+        private System.Windows.Forms.Label Label_log;
     }
 }
 
--- RGBPlot/Form1.cs
+++ RGBPlot/Form1.cs
@@ -12,23 +12,108 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Numerics;
 using System.Windows.Forms.DataVisualization.Charting;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using Aspose.Cells;
 
 namespace RGBPlot {
     public partial class Form1 : Form {
-        public Bitmap bitmap;
+        
         public Form1() {
             InitializeComponent();
+            SetStartPoint(default);
+            SetEndPoint(default);
+            Log("Initilaize completed");
         }
 
-        private void button2_Click(object sender, EventArgs e) {
-            SetStart.Enabled = false;
+        void Log(string message) => Label_log.Text = message;
+
+        Point start, end;
+        public Bitmap bitmap, linedBitmap;
+        (Point pos, Color color)[] colorData = new (Point pos, Color color)[100]; 
+
+        private void DrawChart() {
+            Log("Chart Drawing Started");
+            ChartBox.ChartAreas.Clear();
+            ChartBox.Series.Clear();
+            var area = new ChartArea();
+            area.AxisX.Minimum = 0;
+            area.AxisX.Maximum = colorData.Length;
+            area.AxisY.Minimum = 0;
+            area.AxisY.Maximum = 255;
+            ChartBox.ChartAreas.Add(area);
+
+            Series seriR = new Series() {
+                Color = Color.Red,
+                ChartType = SeriesChartType.Line,
+                LegendText = "R"
+            };
+            Series seriB = new Series() {
+                Color = Color.Blue,
+                ChartType = SeriesChartType.Line,
+                LegendText = "B"
+            };
+            Series seriG = new Series() {
+                Color = Color.Green,
+                ChartType = SeriesChartType.Line,
+                LegendText = "G"
+            };
+
+            for (int i = 0; i < colorData.Length; i++){
+                var c = colorData[i].color;
+                seriR.Points.AddXY(i, c.R);
+                seriB.Points.AddXY(i, c.B);
+                seriG.Points.AddXY(i, c.G);
+            }
+            ChartBox.Series.Add(seriR);
+            ChartBox.Series.Add(seriB);
+            ChartBox.Series.Add(seriG);
+
+            Log("Chart Drawing Completed");
         }
 
-        private void button3_Click(object sender, EventArgs e) {
-            SetEnd.Enabled = false;
+        void GetCopiedBitmap() {
+            linedBitmap?.Dispose();
+            linedBitmap = new Bitmap(bitmap.Width, bitmap.Height);
+            for (int x = 0; x < bitmap.Width; x++){
+                for (int y = 0; y < bitmap.Height; y++){
+                    linedBitmap.SetPixel(x, y, bitmap.GetPixel(x, y));
+                }
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e) {
+        void SetStartPoint(Point point) {
+            start = point;
+            startPos.Text = start.ToString();
+            SetStartBtn.Enabled = true;
+            Log("Start Point was Setted");
+        }
+
+        void SetEndPoint(Point point) {
+            end = point;
+            endPos.Text = end.ToString();
+            SetEndBtn.Enabled = true;
+            Log("End Point was Setted");
+        }
+
+        void DrawLine() {
+            GetCopiedBitmap();
+            float stepX = (end.X - start.X) / (float)colorData.Length,
+                stepY = (end.Y - start.Y) / (float)colorData.Length;
+            for (int i = 0; i < colorData.Length; i++) {
+                var point = new Point((int)(start.X + i * stepX), (int)(start.Y + i * stepY));
+                colorData[i] = (point, bitmap.GetPixel(point.X ,point.Y));
+                for (int j = 0; j < 9; j++){
+                    var x = Math.Max(0, point.X + (j % 3) - 1);
+                    var y = Math.Max(0, point.Y + (j / 3) - 1);
+                    linedBitmap.SetPixel(x,y,Color.Red);
+                }
+            }
+            PictureBox.Image = linedBitmap;
+        }
+
+        private void ChosePictureBtn_Click(object sender, EventArgs e) {
+            Log("Openning FileDialog");
             using (OpenFileDialog openFileDialog = new OpenFileDialog()) {
                 openFileDialog.InitialDirectory = "c:\\";
                 openFileDialog.Filter = "PNG|*.png| JPEG|*.jpg*";
@@ -36,80 +121,92 @@ namespace RGBPlot {
                 openFileDialog.RestoreDirectory = true;
 
                 if (openFileDialog.ShowDialog() == DialogResult.OK) {
-                    //Get the path of specified file
                     var filePath = openFileDialog.FileName;
                     bitmap?.Dispose();
-                    using (var fs = new System.IO.FileStream(filePath,
-                             System.IO.FileMode.Open,
-                             System.IO.FileAccess.Read)) {
+                    using (var fs = new FileStream(filePath,
+                             FileMode.Open,
+                             FileAccess.Read)) {
                         var bmp = new Bitmap(fs);
                         this.filePath.Text = filePath;
-                        double scale_x = ((double)bmp.Width / (double)pictureBox1.Width);
-                        double scale_y = ((double)bmp.Height / (double)pictureBox1.Height);
+                        double scale_x = (double)bmp.Width / (double)PictureBox.Width;
+                        double scale_y = (double)bmp.Height / (double)PictureBox.Height;
                         double scale = (scale_x > scale_y) ? scale_x : scale_y;
 
-                        //リサイズ画像の作成
                         bitmap = new Bitmap(bmp, (int)(bmp.Width / scale), (int)(bmp.Height / scale));
                         bmp.Dispose();
                         fs.Dispose();
-                        pictureBox1.Image = bitmap;
+                        GetCopiedBitmap();
+                        PictureBox.Image = linedBitmap;
+                        Log("Image File Opened");
                     }
-                }
+                }else Log("Canceled FileDialog");
             }
         }
-        Point start, end;
 
-        private void button1_Click_1(object sender, EventArgs e) {
-            if(start == null || end == null || bitmap == null) return;
-            chart1.ChartAreas.Clear();
-            chart1.Series.Clear();
-            chart1.Legends.Clear();
-            chart1.Dock = DockStyle.Fill;
-            var area = new ChartArea();
-            area.InnerPlotPosition.Width = 45f;
-            area.InnerPlotPosition.Height = 50f;
-            area.InnerPlotPosition.X = 50f;
-            chart1.ChartAreas.Add(area);
-
-            Series seriR = new Series() {
-                Color = Color.Red,
-                ChartType = SeriesChartType.Line
-            };
-            Series seriB = new Series() {
-                Color = Color.Blue,
-                ChartType = SeriesChartType.Line
-            };
-            Series seriG = new Series() {
-                Color = Color.Green,
-                ChartType = SeriesChartType.Line
-            };
-            float stepX = (end.X - start.X) / 100f,
-                stepY = (end.Y - start.Y)/100f;
-            for (int i = 0; i < 100; i++){
-                seriR.Points.AddXY(i, bitmap.GetPixel(
-                    (int)(start.X + i * stepX), (int)(start.Y + i * stepY)).R);
-                seriB.Points.AddXY(i, bitmap.GetPixel(
-                    (int)(start.X + i * stepX), (int)(start.Y + i * stepY)).B);
-                seriG.Points.AddXY(i, bitmap.GetPixel(
-                    (int)(start.X + i * stepX), (int)(start.Y + i * stepY)).G);
-            }
-            chart1.Series.Add(seriR);
-            chart1.Series.Add(seriB);
-            chart1.Series.Add(seriG);
+        private void SetStartBtn_Click(object sender, EventArgs e) {
+            if (bitmap == null || !SetEndBtn.Enabled) return;
+            SetStartBtn.Enabled = false;
+            Log("Chose Start Point");
         }
 
-        private void pictureBox1_MouseClick(object sender, MouseEventArgs e) {
+        private void SetEndBtn_Click(object sender, EventArgs e) {
+            if (bitmap == null || !SetStartBtn.Enabled) return;
+            SetEndBtn.Enabled = false;
+            Log("Chose End Point");
+        }
+
+        private void PictureBox_MouseClick(object sender, MouseEventArgs e) {
             if (bitmap is null || e.Location.X > bitmap.Width || e.Location.Y > bitmap.Height) return;
-            if (!SetStart.Enabled) {
-                start = e.Location;
-                startPos.Text = start.ToString();
-                SetStart.Enabled = true;
+            Debug.WriteLine(e.Location);
+            if (!SetStartBtn.Enabled) {
+                SetStartPoint(e.Location);
             }
-            if(!SetEnd.Enabled) {
-                end = e.Location;
-                endPos.Text = end.ToString();
-                SetEnd.Enabled = true;
+            if (!SetEndBtn.Enabled) {
+                SetEndPoint(e.Location);
             }
+            DrawLine();
+        }
+
+        private void PartitionCountBox_ValueChanged(object sender, EventArgs e) {
+            int count = (int)PartitionCountBox.Value;
+            if (count < 10) count = 10;
+            if (count > 500) count = 500;
+            PartitionCountBox.Value = count;
+            colorData = new (Point pos, Color color)[count];
+        }
+
+        private void SaveExcelFile_Click(object sender, EventArgs e) {
+            if (bitmap == null) return;
+            SaveToExcel();
+        }
+
+        private void DrawChartBtn_Click(object sender, EventArgs e) {
+            if (bitmap == null) return;
+            DrawChart();
+        }
+
+        void SaveToExcel() {
+            Log("Creating Xlsx");
+            Workbook wb = new Workbook();
+            Worksheet sheet = wb.Worksheets[0];
+            sheet.Cells["A1"].PutValue("X");
+            sheet.Cells["B1"].PutValue("Y");
+            sheet.Cells["C1"].PutValue("Color");
+            sheet.Cells["D1"].PutValue("R");
+            sheet.Cells["E1"].PutValue("G");
+            sheet.Cells["F1"].PutValue("B");
+            for (int i = 0; i < colorData.Length; i++){
+                string index = (i + 2).ToString();
+                var pos = colorData[i].pos;
+                var c = colorData[i].color;
+                sheet.Cells["A" + index].PutValue(pos.X);
+                sheet.Cells["B" + index].PutValue(pos.Y);
+                sheet.Cells["D" + index].PutValue(c.R);
+                sheet.Cells["E" + index].PutValue(c.G);
+                sheet.Cells["F" + index].PutValue(c.B);
+            }
+            wb.Save("ColorData.xlsx", SaveFormat.Xlsx);
+            Log("Creating Xlsx Completed");
         }
     }
 }
